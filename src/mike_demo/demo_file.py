8a6aec7d40e951539b99870dfaff7ff26c0305dc
Hendrik Scherner <h.scherner@appliedai.de>
2023-12-19 17:56:14 +0100

    Add pytest-cov

--- src/mike_demo/demo_file.py
+++ src/mike_demo/demo_file.py
@@ -16,3 +16,21 @@ def combine_two_string(string_a: str, string_b: str, sep: str = "-") -> str:
         Combined string.
     """
     return string_a + sep + string_b
+
+
+def add_two_ints(int_a: int, int_b: int) -> int:
+    """Add two integers.
+
+    Parameters
+    ----------
+    int_a : int
+        Integer a.
+    int_b : int
+        Integer b.
+
+    Returns
+    -------
+    int
+        Sum of both ints.
+    """
+    return int_a + int_b
